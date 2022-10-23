ed8719b662eb33609d2a088c8e75f22b75880f3a
Harulla <laura.leroychhon@gmail.com>
2022-10-23 17:43:40 +0200

    Premier commit: pages php et feuille de style, tableau

---
+++ page.php
@@ -0,0 +1,80 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Table Style</title>
+	<meta name="viewport" content="initial-scale=1.0; maximum-scale=1.0; width=device-width;">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body> 
+    <div class="table-title">
+        <h3>Data Table</h3>
+    </div>
+    <table class="table-fill">
+        <thead>
+            <tr>
+                <th class="text-left">N°SIREN</th>
+                <th class="text-left">Raison sociale</th>
+                <th class="text-left">Nombre de transactions</th>
+                <th class="text-left">Devise (EUR)</th>
+                <th class="text-left">Montant total</th>
+                <th class="text-left">Raison sociale</th>
+            </tr>
+        </thead>
+
+        <tbody class="table-hover">
+            <tr>
+                <td class="text-left">January</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+            </tr>
+            <tr>
+            <td class="text-left">January</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+            </tr>
+            <tr>
+            <td class="text-left">January</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+            </tr>
+            <tr>
+            <td class="text-left">January</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+            </tr>
+            <tr>
+            <td class="text-left">January</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+                <td class="text-left">$ 50,000.00</td>
+            </tr>
+        </tbody>
+    </table>
+  
+
+  </body>
+<?php
+
+
+
+
+
+
+
+
+?>
