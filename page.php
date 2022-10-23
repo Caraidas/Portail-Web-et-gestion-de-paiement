4043898d10338286dbfd0fc69555f086cb0b9b9d
Harulla <laura.leroychhon@gmail.com>
2022-10-23 19:08:18 +0200

    Création du tableau en pur html css

--- page.php
+++ page.php
@@ -13,55 +13,56 @@
     <table class="table-fill">
         <thead>
             <tr>
-                <th class="text-left">N°SIREN</th>
-                <th class="text-left">Raison sociale</th>
-                <th class="text-left">Nombre de transactions</th>
-                <th class="text-left">Devise (EUR)</th>
-                <th class="text-left">Montant total</th>
-                <th class="text-left">Raison sociale</th>
+                <th>N°SIREN</th>
+                <th>Raison sociale</th>
+                <th>Nombre de transactions</th>
+                <th>Devise</th>
+                <th>Montant total</th>
             </tr>
         </thead>
 
         <tbody class="table-hover">
             <tr>
-                <td class="text-left">January</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
+                <td >362 521 879</td>
+                <td >Renault SAS</td>
+                <td >16</td>
+                <td >EUR</td>
+                <td> 100.0</td>
             </tr>
             <tr>
-            <td class="text-left">January</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
+            <td >357 521 879</td>
+                <td >Laura Leroy SA</td>
+                <td >4</td>
+                <td >DOL</td>
+                <td >102.50</td>
             </tr>
             <tr>
-            <td class="text-left">January</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
+            <td >682 521 879</td>
+                <td >Cloud KICKS</td>
+                <td >25</td>
+                <td >EUR</td>
+                <td >54.20</td>
             </tr>
             <tr>
-            <td class="text-left">January</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
+                <td >124 521 879</td>
+                <td >Patrick Legrand SA</td>
+                <td >2</td>
+                <td >EUR</td>
+                <td >150.0</td>
             </tr>
             <tr>
-            <td class="text-left">January</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
-                <td class="text-left">$ 50,000.00</td>
+                <td >242 521 879</td>
+                <td >FlashDesign SARL</td>
+                <td >254</td>
+                <td >EUR</td>
+                <td >50 000.0</td>
+            </tr>
+            <tr>
+                <td >942 521 879</td>
+                <td >Pokemon SARL</td>
+                <td >14</td>
+                <td >DIR</td>
+                <td >10.0</td>
             </tr>
         </tbody>
     </table>
