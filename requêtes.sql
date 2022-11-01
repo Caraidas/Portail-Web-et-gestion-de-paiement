2ee9014491aeb51203ffe6a619156fdebc64ef7c
Kilouhardi <Joshual.courriel@gmail.com>
2022-11-01 19:40:39 +0100

    vduig gujk
    Mon cervo a fondu et ca marche pas

--- requêtes.sql
+++ requêtes.sql
@@ -1,3 +1,6 @@
+
+
+--N siren|Raison Sociale|Nombre de transaction|Devise|Montant Total--
 SELECT B_Client.NumSiren,
        RaisonSociale,
        B_Client.Devise,
@@ -8,6 +11,54 @@ FROM B_Remise,
      B_Transaction
 WHERE B_Client.NumSiren LIKE B_Remise.NumSiren
   AND B_Remise.NumRemise LIKE B_Transaction.NumRemise
-  AND B_Transaction.Sens LIKE '+'
+  --AND B_Transaction.Sens LIKE '+' Pour moi avoir la tresorerie à besoin de tout types de transaction pour être accurate
+  AND B_Transaction.DateVente = CURDATE() --CURDATE pour avoir la date du jour au format aaaa:mm:jj--
+--AND B_Client.NumSiren = "XXX"--
 GROUP BY NumSiren;
+
+--N siren | Raison sociale | N° Remise | Date traitement | Nbre transactions | Devise (EUR) | Montant total | Sens + ou - --
+
+--test1--
+SELECT B_Client.NumSiren,
+       B_Client.RaisonSociale,
+       B_Client.Devise,
+       B_Remise.NumRemise,
+       B_Remise.DateTraitement,
+       COUNT(B_Transaction.NumAutorisation) AS 'Nombre de transaction' , 
+       MontantsTablePositif.montant - MontantsTableNegatif.montant AS 'Montant Total',
+       '+' AS 'Sens'
+FROM B_Remise,
+     B_Client,
+     B_Transaction,
+     (SELECT SUM(Montant) montant,NumRemise FROM B_Transaction NATURAL JOIN B_Remise WHERE sens = "+" GROUP BY B_Remise.NumRemise)
+     AS MontantsTablePositif,
+     (SELECT SUM(Montant) montant,NumRemise FROM B_Transaction NATURAL JOIN B_Remise WHERE sens = "-" GROUP BY B_Remise.NumRemise)
+     AS MontantsTableNegatif
+WHERE B_Client.NumSiren LIKE B_Remise.NumSiren
+  AND B_Remise.NumRemise LIKE B_Transaction.NumRemise
+  AND MontantsTablePositif.NumRemise LIKE B_Remise.NumRemise
+  AND MontantsTableNegatif.NumRemise LIKE B_Remise.NumRemise
+GROUP BY B_Remise.NumRemise;
+
+--autre facon de lier--
+FROM 
+     B_Client NATURAL JOIN B_Remise NATURAL JOIN B_Transaction NATURAL JOIN ((SELECT SUM(Montant) montant,NumRemise FROM B_Transaction NATURAL JOIN B_Remise WHERE sens = "+" GROUP BY B_Remise.NumRemise)
+     AS MontantsTablePositif) NATURAL JOIN ((SELECT SUM(Montant) montant,NumRemise FROM B_Transaction NATURAL JOIN B_Remise WHERE sens = "-" GROUP BY B_Remise.NumRemise)
+     AS MontantsTableNegatif)
+
+
+--test2--
+SELECT B_Client.NumSiren,
+       B_Client.RaisonSociale,
+       B_Client.Devise,
+       B_Remise.NumRemise,
+       B_Remise.DateTraitement,
+       COUNT(B_Transaction.NumAutorisation) AS 'Nombre de transaction' , 
+       SUM(IF B_Transaction.Sens = '+' SELECT B_Transaction.Montant; ELSE SELECT -(B_Transaction.Montant)) AS 'montant total';
+       '+' AS 'Sens'
+FROM 
+     B_Client NATURAL JOIN B_Remise NATURAL JOIN B_Transaction
+GROUP BY B_Remise.NumRemise;
+
+
 
