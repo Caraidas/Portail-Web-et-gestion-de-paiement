0b3b87d2f72b39fda3fcf4f783112902888c3b92
Harulla <laura.leroychhon@gmail.com>
2022-11-27 21:23:24 +0100

    Page choix de commande admin

--- page.php
+++ page.php
@@ -226,7 +226,7 @@ for ($i = 0; $i < $count - 1; $i++) {
                 </thead>
                 <tbody>";
     echo GenerateHTML::generateDetailsTab($db,$list_remise[$i])[0];
-}
+} 
 ?>
   </body>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
