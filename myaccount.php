b55d632c550b9a499069bb033e1336435dbf21a8
Harulla <laura.leroychhon@gmail.com>
2022-11-28 22:52:14 +0100

    Formulaire my account

--- myaccount.php
+++ myaccount.php
@@ -1,79 +1,119 @@
 <?php
-require_once 'Class/Database.php';
-require_once 'Class/SQLData.php';
-session_start();
-if (isset($_SESSION['id']) && isset($_SESSION['role'])){
+    require_once 'Class/Database.php';
+    require_once 'Class/SQLData.php';
+    session_start();
+?>
 
-    echo 'Votre login est : '.$_SESSION['id'].'<br> Votre role est : ' .$_SESSION['role'];
-    //vérification qu'il s'agit bien de l'admin
-    if ($_SESSION['role'] == 'Admin'){
-        $db = Database::getPDO();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='style-connexion.css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        echo '<div class="container"><h1 id="title">Ajouter un utilisateur</h1>';
+        if (isset($_SESSION['id']) && isset($_SESSION['role'])){
+            //vérification qu'il s'agit bien de l'admin
+            if ($_SESSION['role'] == 'Admin'){
+                $db = Database::getPDO();
 
 
-        //vérification que les champs pour la suppression d'un login est bien entré
+                //vérification que les champs pour la suppression d'un login est bien entré
 
-        if (isset($_POST['Login']) && !empty($_POST['Login'])) {
-            if(isset($_POST['checkbox'])){
-                SQLData::deleteUser($db, $_POST['Siren']);
-                echo "<br>l'utilisateur " . $_POST['Siren'] . "a bien été supprimé<br>";
-            }
-            else
-                echo"l'accord du PO n'a pas été accordé";
-        }
-
-        //vérification que les champs pour l'ajout d'un login sont bien entré
-        if (isset($_POST["AjoutMDP"]) && ($_POST["AjoutMDP"] === $_POST["ConfirmationMDP"]) && !empty($_POST["AjoutMDP"])) {
-            if (isset($_POST["AjoutLogin"]) && !empty($_POST["AjoutLogin"])){
-                if(isset($_POST['checkbox'])){
-                    SQLData::addLogin($db, $_POST["AjoutLogin"], $_POST["AjoutRole"], $_POST["AjoutMDP"]);
-                    echo "<br>l'utilisateur " . $_POST['AjoutLogin'] . "a bien été ajouté<br>";
+                if (isset($_POST['Login']) && !empty($_POST['Login'])) {
+                    if(isset($_POST['checkbox'])){
+                        SQLData::deleteUser($db, $_POST['Siren']);
+                        echo "<br>l'utilisateur " . $_POST['Siren'] . "a bien été supprimé<br>";
+                    }
+                    else
+                        echo"l'accord du PO n'a pas été accordé";
                 }
-               else
-                   echo"l'accord du PO n'a pas été accordé";
+
+                //vérification que les champs pour l'ajout d'un login sont bien entré
+                if (isset($_POST["AjoutMDP"]) && ($_POST["AjoutMDP"] === $_POST["ConfirmationMDP"]) && !empty($_POST["AjoutMDP"])) {
+                    if (isset($_POST["AjoutLogin"]) && !empty($_POST["AjoutLogin"])){
+                        if(isset($_POST['checkbox'])){
+                            SQLData::addLogin($db, $_POST["AjoutLogin"], $_POST["AjoutRole"], $_POST["AjoutMDP"]);
+                            echo "<div class='success'><p>L'utilisateur " . $_POST['AjoutLogin'] . " a bien été ajouté</p></div>";
+                        }
+                    }
+                }
+
+
+                $clients = SQLData::getLogin($db,$_SESSION['role']);
+
+                
+                echo '
+                    <form class="center" id="Form" method="POST" action="">
+                    <div>
+                        <label for="AjoutLogin">Identifiant</label>
+                        <input class="input" id="identifiant" name="AjoutLogin" type="text" placeholder="Entrez Login" required/>
+                    </div>
+                    <div>
+                        <label for="AjoutRole">Rôle</label>
+                        <select class="input" id="role" name="AjoutRole">
+                            <option valeur="Admin">Admin</option>
+                            <option valeur="PO">Product Owner</option>
+                            <option valeur="Commerçant">Commerçant</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label class="pswrd" for="AjoutMDP">Mot de passe</label>
+                        <div>
+                            <input class="input" id="id_password_first" name="AjoutMDP" type="password" placeholder="Entrez le mot de passe" required />
+                            <i class="far fa-eye" id="togglePasswordFirst" style="position: absolute; margin: 19px -40px; cursor: pointer; color: var(--bleu);"></i>
+                        </div>
+                        <script>
+                            const togglePassword = document.querySelector("#togglePasswordFirst");
+                            const password = document.querySelector("#id_password_first");
+
+                            togglePassword.addEventListener("click", function(e) {
+                                // toggle the type attribute
+                                const type = password.getAttribute("type") === "password" ? "text" : "password";
+                                password.setAttribute("type", type);
+                                // toggle the eye slash icon
+                                this.classList.toggle("fa-eye-slash");
+                            });
+                        </script>
+                    </div>
+                    <div>
+                        <label class="pswrd" for="ConfirmationMDP">Confirmation</label>
+                        <div>
+                            <input class="input" id="id_password_second" name="ConfirmationMDP" type="password" placeholder="Confirmer le mot de passe" required/>
+                            <i class="far fa-eye" id="togglePasswordSecond" style="position: absolute; margin: 19px -40px; cursor: pointer; color: var(--bleu);"></i>
+                        </div>
+                        <script>
+                            const togglePassword2 = document.querySelector("#togglePasswordSecond");
+                            const password2 = document.querySelector("#id_password_second");
+
+                            togglePassword2.addEventListener("click", function(e) {
+                                // toggle the type attribute
+                                const type2 = password2.getAttribute("type") === "password" ? "text" : "password";
+                                password2.setAttribute("type", type2);
+                                // toggle the eye slash icon
+                                this.classList.toggle("fa-eye-slash");
+                            });
+                        </script>
+                    </div>
+                    <div>
+                        <label style="color:black; font-size:14px;padding-left:0px" for="checkbox"> accord du PO</label>
+                        <input type="checkbox" id="checkbox" name="checkbox" value="1" required>
+                    </div>
+                    <button class="submit" id="Button" type="submit">Créer</button></form></div>';
             }
-            else
-                echo "champs login manquant";
+
+
+        }else{// pas encore connecté
+            header('Location: connexion-user.php');
         }
+    ?>
 
-
-        $clients = SQLData::getLogin($db,$_SESSION['role']);
-        while($row = $clients->fetch(PDO::FETCH_ASSOC)){
-            echo "<br><tr><td>".$row['Login']." <br></td></tr>";
-        }
-        echo '<br> Pour supprimer un Client, entrez son Siren ainsi que son Login si-dessous';
-        echo'<form id="Form" method="POST" action="">
-             <input id="name" name="Login" type="text" placeholder="Entrez Login" data-sb-validations="required" />
-             <label for="Login">Login</label>
-             <button id="Button" type="submit">supprimer</button>';
-
-        echo '<br> Pour ajouter un utilisateur, entrez son Login ainsi que son Role puis le mot de passe si-dessous';
-        echo '<form id="Form" method="POST" action=""><br>
-             <input id="name" name="AjoutLogin" type="text" placeholder="Entrez Login" data-sb-validations="required"/>
-             <label for="AjoutLogin">AjoutLogin</label>
-             <br>
-             <select id="name" name="AjoutRole">
-                <option valeur="Admin">Admin</option>
-                <option valeur="PO">PO</option>
-                <option valeur="Commerçant">Commerçant</option>
-             </select>
-             <label for="AjoutRole">Ajout Role</label>
-             <br>
-             <label for="AjoutMDP">mot de passe</label>
-             <br>
-             <input id="name" name="AjoutMDP" type="password" placeholder="Entrez mdp" data-sb-validations="required" />
-             <br>
-             <label for="ConfirmationMDP">confirmation mot de passe</label>
-             <br>
-             <input id="name" name="ConfirmationMDP" type="password" placeholder="Entrez mdp" data-sb-validations="required" />
-             <br>
-             <label for="addUser">Ajouter l\'utilisateur</label>
-             <br>
-             <button id="Button" type="submit">Créer</button>
-             <input type="checkbox" id="checkbox" name="checkbox" value="1">
-             <label for="checkbox"> accord du PO</label>';
-    }
-
-
-}else{// pas encore connecté
-    header('Location: connexion-user.php');
-}
+</body>
+</html>
